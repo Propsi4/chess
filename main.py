93867e3b1caf7ecd8fefb6dd4fb124010923505c
Propsi4 <gaenday12@gmail.com>
2022-10-24 21:53:52 +0300

    v1

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from PyQt6 import uic
 from PyQt6 import QtCore
-from PyQt6.QtWidgets import QApplication, QTableWidgetItem,QMainWindow,QTableWidget
-from PyQt6.QtGui import QColor
-import sys
-allowed = ('П','Т','К','С','Ф','Кр','п','т','к','с','ф','кр','КР')
+from PyQt6.QtWidgets import QApplication, QTableWidgetItem,QMainWindow,QTableWidget, QWidget
+from PyQt6.QtGui import QColor, QPainter, QPixmap
+import sys, traceback
+allowed = ('П1','Т1','Кі1','С1','Ф1','Кр1','П2','Т2','Кі2','С2','Ф2','Кр2')
+class ImageWidget(QWidget):
+
+    def __init__(self, imagePath, parent):
+        super(ImageWidget, self).__init__(parent)
+        self.picture = QPixmap(imagePath)
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.drawPixmap(0, 0, self.picture)
 class App(QMainWindow):
 	def __init__(self):
 		super(App, self).__init__()
@@ -13,6 +22,45 @@ class App(QMainWindow):
 		self.old_text = ''
 		self.show() # Show the GUI
 		self.prepare()
+
+
+	def setImage(self, row, col, text):
+		if (text.__contains__("П")):
+			if (text.__contains__("1")):
+				path = 'resized_sprites/wp.png'
+			else:
+				path = 'resized_sprites/bp.png'
+		elif (text.__contains__("Т")):
+			if (text.__contains__("1")):
+				path = 'resized_sprites/wr.png'
+			else:
+				path = 'resized_sprites/br.png'
+		elif (text.__contains__("Кі")):
+			if (text.__contains__("1")):
+				path = 'resized_sprites/wn.png'
+			else:
+				path = 'resized_sprites/bn.png'
+		elif (text.__contains__("С")):
+			if (text.__contains__("1")):
+				path = 'resized_sprites/wb.png'
+			else:
+				path = 'resized_sprites/bb.png'
+		elif (text.__contains__("Ф")):
+			if (text.__contains__("1")):
+				path = 'resized_sprites/wq.png'
+			else:
+				path = 'resized_sprites/bq.png'
+		elif (text.__contains__("Кр")):
+			if (text.__contains__("1")):
+				path = 'resized_sprites/wk.png'
+			else:
+				path = 'resized_sprites/bk.png'
+		else:
+			path = None
+		image = ImageWidget(path, self.board)
+		self.board.setCellWidget(row, col, image)
+	def remImage(self, row, col):
+		self.board.setCellWidget(row, col, None)
 	def prepare(self):
 		# Виконується тільки один раз, підготовка таблиці і тд
 		self.board.cellClicked.connect(self.selected)
@@ -21,18 +69,18 @@ class App(QMainWindow):
 		self.board.setEditTriggers(QTableWidget.EditTrigger.NoEditTriggers)
 		for i in range(8):
 			self.board.setColumnWidth(i,59)
-			self.board.setRowHeight(i,58)
+			self.board.setRowHeight(i,60)
 			for k in range(8):
 				item = QTableWidgetItem()
 				item.setTextAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
 				self.board.setItem(i,k,item)
 				if((k+i)%2!=0):
 					self.board.item(i,k).setBackground(QColor("#000000"))
-					self.board.item(i,k).setForeground(QColor("#ffffff"))
+					self.board.item(i,k).setForeground(QColor("#000000"))
 
 				else:
 					self.board.item(i, k).setBackground(QColor("#ffffff"))
-					self.board.item(i, k).setForeground(QColor("#000000"))
+					self.board.item(i, k).setForeground(QColor("#ffffff"))
 		self.reset()
 		self.board.itemChanged.connect(self.change)
 	def change(self):
@@ -41,7 +89,10 @@ class App(QMainWindow):
 		text = self.board.item(row,column).text()
 		if(text not in allowed):
 			self.board.item(row,column).setText('')
-
+			self.remImage(row, column)
+		else:
+			self.board.item(row, column).setText(text)
+			self.setImage(row, column, text)
 	def edit_mode(self):
 		# Режим ручного введення кліток
 		if(self.edit_button.isChecked()):
@@ -49,26 +100,40 @@ class App(QMainWindow):
 		else:
 			self.board.setEditTriggers(QTableWidget.EditTrigger.NoEditTriggers)
 	def reset(self):
+		# Очистка поля
+		for i in range(8):
+			for k in range(8):
+				self.board.item(i,k).setText('')
+				self.remImage(i,k)
 		# Розстановка стартових фігур
 		for i in range(8):
-			self.board.item(1,i).setText("П")
-			self.board.item(6,i).setText("П")
-		self.board.item(7,0).setText("Т")
-		self.board.item(7,7).setText("Т")
-		self.board.item(0,7).setText("Т")
-		self.board.item(0,0).setText("Т")
-		self.board.item(0,1).setText("К")
-		self.board.item(0,6).setText("К")
-		self.board.item(7,1).setText("К")
-		self.board.item(7,6).setText("К")
-		self.board.item(0,2).setText("С")
-		self.board.item(0,5).setText("С")
-		self.board.item(7,2).setText("С")
-		self.board.item(7,5).setText("С")
-		self.board.item(7,3).setText("Ф")
-		self.board.item(0,3).setText("Ф")
-		self.board.item(7,4).setText("Кр")
-		self.board.item(0,4).setText("Кр")
+			self.board.item(6,i).setText("П1")
+			self.board.item(1,i).setText("П2")
+		# Команда білих
+		self.board.item(7,0).setText("Т1")
+		self.board.item(7,7).setText("Т1")
+		self.board.item(7,1).setText("Кі1")
+		self.board.item(7,6).setText("Кі1")
+		self.board.item(7,2).setText("С1")
+		self.board.item(7,5).setText("С1")
+		self.board.item(7,3).setText("Ф1")
+		self.board.item(7,4).setText("Кр1")
+		# Команда чорних
+		self.board.item(0,7).setText("Т2")
+		self.board.item(0,0).setText("Т2")
+		self.board.item(0,1).setText("Кі2")
+		self.board.item(0,6).setText("Кі2")
+		self.board.item(0,2).setText("С2")
+		self.board.item(0,5).setText("С2")
+		self.board.item(0,3).setText("Ф2")
+		self.board.item(0,4).setText("Кр2")
+		for i in range(8):
+			for k in range(8):
+				text = self.board.item(i,k).text()
+				self.setImage(i,k,text)
+
+
+
 	def current_cell(self):
 		row = self.board.currentRow()
 		column = self.board.currentColumn()
@@ -77,21 +142,31 @@ class App(QMainWindow):
 		# Рух фігури
 		self.board.item(from_row,from_column).setText('')
 		self.board.item(to_row,to_column).setText(figure)
+		# Переміщення картинки
+		self.remImage(from_row,from_column)
+
 	def selected(self):
 		# Допоміжна функція move
 		row, column = self.current_cell()
 		text = self.board.item(row,column).text()
-		if (self.old_text != text and text == ''):
+		if (self.old_text != text and text == "" and not self.edit_button.isChecked()):
 			self.move(row, column, self.old_row, self.old_column, self.old_text)
-			#pass
+		if(self.edit_button.isChecked()):
+			self.remImage(row,column)
+			self.board.item(row,column).setText('')
 		self.old_row = row
 		self.old_column = column
 		self.old_text = text
-
+def excepthook(exc_type, exc_value, exc_tb):
+    tb = "".join(traceback.format_exception(exc_type, exc_value, exc_tb))
+    print("error catched!:")
+    print("error message:\n", tb)
+    file = open("error.txt","w")
+    file.write(tb)
+    file.close()
+    app.quit()
 if __name__ == "__main__":
+	sys.excepthook = excepthook
 	app = QApplication(sys.argv)
 	window = App()
 	app.exec()
-#
-
-
